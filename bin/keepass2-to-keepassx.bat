833c9b38f9ed8668bbb2b20ef7ea22da521f63b8
Martin Dvorak <martin.dvorak@gmail.com>
2014-10-14 08:57:06 +0200

    Adding BAT runner (written on Unix and not tested).

---
+++ bin/keepass2-to-keepassx.bat
@@ -0,0 +1,35 @@
+@echo off
+
+@REM Licensed to the Apache Software Foundation (ASF) under one
+@REM or more contributor license agreements.  See the NOTICE file
+@REM distributed with this work for additional information
+@REM regarding copyright ownership.  The ASF licenses this file
+@REM to you under the Apache License, Version 2.0 (the
+@REM "License"); you may not use this file except in compliance
+@REM with the License.  You may obtain a copy of the License at
+
+@REM   http://www.apache.org/licenses/LICENSE-2.0
+
+@REM Unless required by applicable law or agreed to in writing,
+@REM software distributed under the License is distributed on an
+@REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+@REM KIND, either express or implied.  See the License for the
+@REM specific language governing permissions and limitations
+@REM under the License.
+
+:checkJava
+set _JAVACMD=%JAVACMD%
+
+if "%JAVA_HOME%" == "" goto noJavaHome
+if not exist "%JAVA_HOME%\bin\java.exe" goto noJavaHome
+if "%_JAVACMD%" == "" set _JAVACMD=%JAVA_HOME%\bin\java.exe
+goto endcommon
+
+:noJavaHome
+if "%_JAVACMD%" == "" set _JAVACMD=java.exe
+
+:endcommon
+
+"%_JAVACMD%" -classpath "..\lib\keepass2-to-keepassx.jar:..\lib\xpp3-1.1.4c.jar" com.mindforger.keepass.Keepass2DatabaseToKeepassXDatabase %1 %2
+
+@REM eof
